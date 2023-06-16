6d85074163c555eb6d5beeb56671d3e8caf28ab2
AaronOSI <aaron.alcarde@opensoftsolutions.com>
2023-06-16 12:04:25 +0800

    nextAuth

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import { NextAuthOptions }  from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredenialsProvider from 'next-auth/providers/credentials'
+import NextAuth from 'next-auth/next'
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+
+    ],
+    secret: process.env.SECRET
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from 'next/image'
+'use client';
 
+import Image from 'next/image'
+import GoogleButton from 'react-google-button'
+import { signIn } from 'next-auth/react'
 
 export default function Home() {
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-xl">Welcome to Homepage!</h1>
+      <GoogleButton onClick={()=>{signIn('google')}} className="mx-auto mt-16" />
     </main>
   )
 }
